497f705cde3677e3e8691a4b85a24d7f38cfd7d4
skyler <konata.izumi20@gmail.com>
2020-07-26 19:51:06 -0400

    Add basic player movement.

---
+++ Can'tWriteJam2020/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform toFollow;
+    public float fixedY = 0f;
+    public float smoothTime = 0.3F;
+    private Vector3 velocity = Vector3.zero;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Define a target position above and behind the target transform
+        Vector3 targetPosition = toFollow.TransformPoint(offset);
+        targetPosition.y = fixedY;
+
+        // Smoothly move the camera towards that target position
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
---
+++ Can'tWriteJam2020/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        rb2d.velocity = new Vector2(x * speed, y * speed);
+    }
+}
